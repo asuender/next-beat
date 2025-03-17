ca29bb5352f3cf79e38ea729186f3370f5fb0e13
Andreas Sünder <andreas.suender@icloud.com>
2025-03-17 17:22:19 +0100

    Move header to layout.tsx

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -13,43 +13,29 @@ export default async function Board() {
   const topTracks = await getUserTop(session, 'artists');
 
   return (
-    <>
-      <header className="flex justify-between items-center px-5">
-        <h1 className="text-xl font-bold my-4">🎹 Next Board</h1>
-        <button
-          onClick={async () => {
-            'use server';
-            await signOut({ redirectTo: '/' });
-          }}
-        >
-          Sign out
-        </button>
-      </header>
-
-      <main className="w-full">
-        <div className="max-w-5xl mx-auto">
-          <div className="flex justify-between items-center mt-16 mb-8 px-2">
-            <h2 className="text-4xl font-bold">
-              {session.user?.name}'s Top Artists
-            </h2>
-            <Image
-              src={session.user?.image || ''}
-              alt="User logo"
-              className="w-10 h-10 rounded-full"
-              width={40}
-              height={40}
-            />
-          </div>
-
-          {topTracks && (
-            <div className="lg:columns-3 md:columns-2 columns-1">
-              {topTracks.items.map((artist: ArtistObject) => (
-                <Artist key={artist.id} {...artist} />
-              ))}
-            </div>
-          )}
+    <main className="w-full">
+      <div className="max-w-5xl mx-auto">
+        <div className="flex justify-between items-center mt-16 mb-8 px-2">
+          <h2 className="text-4xl font-bold">
+            {session.user?.name}'s Top Artists
+          </h2>
+          <Image
+            src={session.user?.image || ''}
+            alt="User logo"
+            className="w-10 h-10 rounded-full"
+            width={40}
+            height={40}
+          />
         </div>
-      </main>
-    </>
+
+        {topTracks && (
+          <div className="lg:columns-3 md:columns-2 columns-1">
+            {topTracks.items.map((artist: ArtistObject) => (
+              <Artist key={artist.id} {...artist} />
+            ))}
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
 import './globals.css';
+import { signOut } from 'next-auth/react';
 
 const rubik = Rubik({
   subsets: ['latin'],
@@ -8,7 +9,8 @@ const rubik = Rubik({
 
 export const metadata: Metadata = {
   title: 'Next Beat',
-  description: 'A small web app showing the most listened tracks of a user, combining the Next.js framework with the heartbeat of your music!',
+  description:
+    'A small web app showing the most listened tracks of a user, combining the Next.js framework with the heartbeat of your music!',
 };
 
 export default function RootLayout({
@@ -18,7 +20,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${rubik.className} antialiased`}>{children}</body>
+      <body className={`${rubik.className} antialiased`}>
+        <header className="flex justify-between items-center px-5">
+          <h1 className="text-xl font-bold my-4">🎹 Next Board</h1>
+          <button
+            onClick={async () => {
+              'use server';
+              await signOut({ redirectTo: '/' });
+            }}
+          >
+            Sign out
+          </button>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
