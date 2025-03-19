31369cdd59a852124bfad25bac2c15eba7677514
Andreas Sünder <andreas.suender@icloud.com>
2025-03-19 06:45:44 +0100

    Refactor MotionButton

--- app/board/layout.tsx
+++ app/board/layout.tsx
@@ -1,56 +1,64 @@
 import Link from 'next/link';
 import MotionLink from '@/app/components/MotionLink';
 import { signOut } from '@/auth';
-import SignOutButton from '@/app/components/SignOutButton';
+import MotionButton from '@/app/components/MotionButton';
 
-export default function BoardLayout({ children }: { children: React.ReactNode }) {
+export default function BoardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <header className="flex justify-between items-center px-5">
-          <div className="flex gap-10">
-            <Link href="/" className="text-xl font-bold my-4">
-              🎹 Next Board
-            </Link>
-            <ul className="flex items-center gap-5">
-              <li>
-                <MotionLink href="/board/tracks" content="Tracks" />
-              </li>
-              <li>
-                <MotionLink href="/board/artists" content="Artists" />
-              </li>
-              <li>
-                <MotionLink href="/board/podcasts" content="Podcasts" />
-              </li>
-            </ul>
-          </div>
+        <div className="flex gap-10">
+          <Link href="/" className="text-xl font-bold my-4">
+            🎹 Next Board
+          </Link>
+          <ul className="flex items-center gap-5">
+            <li>
+              <MotionLink href="/board/tracks" content="Tracks" />
+            </li>
+            <li>
+              <MotionLink href="/board/artists" content="Artists" />
+            </li>
+            <li>
+              <MotionLink href="/board/podcasts" content="Podcasts" />
+            </li>
+          </ul>
+        </div>
 
-          <SignOutButton onClick={async () => {
+        <MotionButton
+          onClick={async () => {
             'use server';
-            await signOut({ redirectTo: '/' }); 
-          }} />
-        </header>
+            await signOut({ redirectTo: '/' });
+          }}
+        >
+          Sign out
+        </MotionButton>
+      </header>
 
-        {children}
+      {children}
 
-        <footer className="flex items-center justify-center text-gray-500 mt-16 mb-4">
-          <p>
-            Made by{' '}
-            <a
-              href="https://github.com/asuender"
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
-            >
-              asuender
-            </a>
-            . Created with{' '}
-            <a
-              href="https://nextjs.org"
-              className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
-            >
-              Next.js
-            </a>
-            .
-          </p>
-        </footer>
+      <footer className="flex items-center justify-center text-gray-500 mt-16 mb-4">
+        <p>
+          Made by{' '}
+          <a
+            href="https://github.com/asuender"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
+          >
+            asuender
+          </a>
+          . Created with{' '}
+          <a
+            href="https://nextjs.org"
+            className="text-blue-500 hover:text-blue-600 dark:text-blue-400"
+          >
+            Next.js
+          </a>
+          .
+        </p>
+      </footer>
     </>
-  )
+  );
 }
--- app/components/MotionButton.tsx
+++ app/components/MotionButton.tsx
@@ -2,14 +2,14 @@
 
 import { motion } from 'motion/react';
 
-export default function MotionButton({ onClick }: { onClick: () => void }) {
+export default function MotionButton({ onClick, children }: { onClick: () => void, children: React.ReactNode }) {
   return (
     <motion.button
       onClick={() => onClick()}
       whileHover={{ opacity: 0.3, transition: { duration: 0.3 } }}
       className="cursor-pointer"
     >
-      Sign out
+      {children}
     </motion.button>
   )
 }
